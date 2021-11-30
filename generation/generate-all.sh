8039d4c439f97f0236fefccd160371eafbf2a8d7
Daniel Lamando <dan@danopia.net>
2021-11-30 09:48:14 +0100

    Add API surface for Argo CD's CRDs

--- generation/generate-all.sh
+++ generation/generate-all.sh
@@ -2,5 +2,6 @@
 
 ./generation/sources/builtin.sh v1.22.4
 
+./generation/sources/argo-cd.sh v2.1.7
 ./generation/sources/cert-manager.sh v1.6.1
 ./generation/sources/external-dns.sh v0.10.1
---
+++ generation/sources/argo-cd.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -eux
+
+gitapi="https://api.github.com"
+upstream="argoproj/argo-cd"
+crdpath="manifests/crds"
+projectname="argo-cd"
+specdir="generation/api-specs/$projectname-$1"
+
+if [ ! -d "$specdir" ]
+then {
+  echo 'mkdir '"$specdir"
+  echo 'cd '"$specdir"
+  wget -O - "$gitapi/repos/$upstream/contents/$crdpath?ref=$1" \
+  | jq -r '.[] | select(.name | endswith("-crd.yaml")) | "wget \(.download_url)"'
+} | sh -eux
+fi
+
+mkdir -p "lib/$projectname"
+rm -r "lib/$projectname"/* \
+|| true
+
+deno run \
+  --allow-read="generation/api-specs" \
+  --allow-write="lib/$projectname" \
+  generation/run-on-crds.ts \
+  "$specdir" \
+  "$projectname"
+
+deno cache "lib/$projectname"/*/mod.ts
+git status "lib/$projectname"
