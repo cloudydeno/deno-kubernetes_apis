ddf7fb183aa8d95cdf7320d4e80a23d10da74d11
Daniel Lamando <dan@danopia.net>
2024-09-18 22:49:21 +0200

    Update release notes

--- lib/deps.ts
+++ lib/deps.ts
@@ -2,10 +2,10 @@
 // kubernetes_apis itself only depends on specific files,
 // so this is provided an optional utility
 
-export * from "https://deno.land/x/kubernetes_client@v0.7.0/mod.ts";
-export * as tunnelBeta from "https://deno.land/x/kubernetes_client@v0.7.0/tunnel-beta/via-websocket.ts";
+export * from "https://deno.land/x/kubernetes_client@v0.7.3/mod.ts";
+export * as tunnelBeta from "https://deno.land/x/kubernetes_client@v0.7.3/tunnel-beta/via-websocket.ts";
 
-export * from "https://deno.land/x/kubernetes_client@v0.7.0/lib/contract.ts";
+export * from "https://deno.land/x/kubernetes_client@v0.7.3/lib/contract.ts";
 export {
   WatchEventTransformer,
-} from "https://deno.land/x/kubernetes_client@v0.7.0/lib/stream-transformers.ts";
+} from "https://deno.land/x/kubernetes_client@v0.7.3/lib/stream-transformers.ts";
